b4d278dabb76d4448294f326c54a6a738e7089d9
vjkrsh <33751890+vjkrsh@users.noreply.github.com>
2024-02-08 23:03:04 -0600

    updated package name

--- src/main/java/org/group17_cs4050_assignment1/Main.java
+++ src/main/java/org/group17_cs4050_assignment1/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.group17_cs4050_assignment1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
